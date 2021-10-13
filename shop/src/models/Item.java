5e50f0bc99405467d10bb68428e3ce1ea433bbff
Choi <Choi@121.162.11.220>
2021-10-13 19:32:51 +0900

    dev

--- shop/src/models/Item.java
+++ shop/src/models/Item.java
@@ -1,14 +1,14 @@
 package models;
 
 public class Item {
-	String name;
-	int price;
-	String category; // 카테고리 // 육류, 과자, 어류, 과일 등등
+	private String name;
+	private int price;
+	private String category; // 카테고리 // 육류, 과자, 어류, 과일 등등
 	
-	public Item(String na, int pr, String cate){
-		name = na;
-		price = pr;
-		category = cate;
+	public Item(String name, int price, String category){
+		this.name = name;
+		this.price = price;
+		this.category = category;
 	}
 	
 	void print() {
